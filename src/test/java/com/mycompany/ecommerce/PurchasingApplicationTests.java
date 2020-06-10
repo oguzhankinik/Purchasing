0159dd1f2f25a899a6868984fbc03bdb2685f7db
Oğuzhan Kinik <oguzhan.kinik@btc-ag.com.tr>
2020-06-10 23:02:01 +0300

    initial commit

---
+++ src/test/java/com/mycompany/ecommerce/PurchasingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mycompany.ecommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PurchasingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
